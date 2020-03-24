8542c633490db0910eac904a4dd8edb192819968
tony <tony@tony.tc>
2020-03-24 05:32:01 +0000

    Use spotipy for authentication

--- spotigen.py
+++ spotigen.py
@@ -1,10 +1,13 @@
-import json
+import sys
 from jinja2 import Template
+import spotipy
 
 if __name__ == "__main__":
-    artists = json.load(open("test-artists.json"))["items"]
+    token = sys.argv[1]
+    sp = spotipy.Spotify(auth=token)
+    artists = sp.current_user_top_artists(time_range="short_term", limit=10)["items"]
     artists = [artists[:5], artists[5:]]
-    albums = json.load(open("test-tracks.json"))["items"]
+    albums = sp.current_user_top_tracks(time_range="short_term", limit=10)["items"]
     albums = [albums[:5], albums[5:]]
     with open("template.html") as f:
         template = Template(f.read())
