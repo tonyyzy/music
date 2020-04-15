8a699a9d14e375c611eee2f83ab8af8affbd77ca
Tony Yang <tony@tony.tc>
2020-04-15 09:50:03 +0100

    change time range for tracks

--- spotigen.py
+++ spotigen.py
@@ -12,7 +12,7 @@ if __name__ == "__main__":
     sp = spotipy.Spotify(auth=token)
     artists = sp.current_user_top_artists(time_range="short_term", limit=10)["items"]
     artists = [artists[:5], artists[5:]]
-    albums = sp.current_user_top_tracks(time_range="short_term", limit=10)["items"]
+    albums = sp.current_user_top_tracks(time_range="medium_term", limit=10)["items"]
     albums = [albums[:5], albums[5:]]
     with open("template.html") as f:
         template = Template(f.read())
